0a718a138df0197b0ca08c684cc91c8a5914f0a8
Alswell <alswelleunice@gmail.com>
2023-09-08 10:45:57 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const moment = require("moment-timezone")
+
+const app = express();
+
+const HOST = "localhost";
+const PORT = "8080";
+
+app.get("/",(req,res)=>{
+    const slackName = req.query.slack_name||"Eunice Gyau";
+    const track = req.query.track || "Backend";
+
+    const dayOfWeek = moment().tz("UTC").format("dddd");
+    const currentUTCtime = moment().tz('UTC').format("YYYY-MM-DDTHH:mm:ss[Z]")
+
+    const githubFileURL = "https://github.com/eunice-alswell/slack-info-endpoint.git";
+    const githubRepoURL = "https://github.com/eunice-alswell/slack-info-endpoint.git";
+
+    const data = {
+        "slack_name" : slackName,
+        "current_day" : dayOfWeek,
+        "utc_time" : currentUTCtime,
+        "track" : track,
+        "github_file_url":githubFileURL,
+        "github_repo_url" : githubRepoURL,
+        "status" : 200
+    };
+
+    res.status(200).json(data)
+})
+
+app.listen(PORT,()=>{
+    console.log(`Server has started on ${HOST}: ${PORT}`)
+})
